e9625b74c0026b66baf05796a5772df7d06f7056
Omar Jones <haitiboi7969@gmail.com>
2020-08-12 03:00:02 -0400

    created auths webtokens and profile

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
+const { request } = require('express');
+
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
-
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     });
 
     console.log('MongoDB Connected...');
--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-// Connect Database
+// connect database
 connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'));
 
-//Define Routes
+// Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
 
 const PORT = process.env.PORT || 5000;
 
